f19c745a953fc7585715feb109549d84833e93c8
diego_juarez_velasco <diegoaw591@gmail.com>
2020-12-14 21:14:16 -0600

    add login POST route

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-
 const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
 app.get("/users", function (req, res) {
   res.json({
     success: true,
     message: "successfully got users. Nice!",
     users: mockUserData,
   });
-});
-app.listen(8000, function () {
-  console.log("Server is running");
 });
 app.get("/users/:id", function (req, res) {
   console.log(req.params.id);
@@ -20,3 +20,26 @@ app.get("/users/:id", function (req, res) {
     user: req.params.id,
   });
 });
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "password and username match!",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match",
+    });
+  }
+});
+app.listen(8000, function () {
+    console.log("Server is running");
+  });
